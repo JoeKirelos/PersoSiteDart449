a13c70c8b79e74da0c7a2fff87ab4895f16488f9
JOE KIRELOS <joekirelos@gmail.com>
2022-01-30 17:21:53 -0500

    init

---
+++ assignment class 3/js/main.js
@@ -0,0 +1,53 @@
+const hamBtn = document.querySelector('#hamburgerBtn');
+const hamMenu = document.querySelector('div.hamMenu');
+const firstPotato = document.querySelector('#firstPotato');
+const barText = document.querySelector('#barText');
+let potatoes = [];
+const containter = document.querySelector('#contentContainer');
+const addBtn = document.querySelector('#addBtn');
+hamBtn.onclick =  function(){
+    console.log("clicked");
+    hamMenu.classList.toggle('hamExpanded');
+}
+
+firstPotato.onmouseenter = function(){
+    firstPotato.classList.add('potatoHovered');
+}
+firstPotato.onmouseleave =function(){
+    firstPotato.classList.remove('potatoHovered');
+}
+
+addBtn.onclick = function(){
+    let newPotato = document.createElement('img');
+    newPotato.src = "../assets/potato.png";
+    newPotato.classList.add('potImg');
+    containter.appendChild(newPotato);
+    potatoes = document.querySelectorAll('img.potImg');
+    potatoes.forEach(element => {
+        element.onmouseenter = function(){
+            element.classList.add('potatoHovered');
+        }
+        element.onmouseleave =function(){
+            element.classList.remove('potatoHovered');
+        }
+    });
+    barText.textContent = `There are currently ${potatoes.length} potatoes`;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
